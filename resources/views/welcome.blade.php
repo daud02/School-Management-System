d953084bde8f6ddce63d5617d3494661320c64ec
niloy <122288265+niloy2107028@users.noreply.github.com>
2025-10-10 13:15:31 +0600

    Tab Tittles and logo added

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,6 +5,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>I.E.T Government High School Management System</title>
+    <link rel="icon" type="image/png" href="{{ asset('images/logo.jpg') }}">
+    <link rel="shortcut icon" type="image/png" href="{{ asset('images/logo.jpg') }}">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet">
     <style>
